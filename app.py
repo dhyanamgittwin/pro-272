5b3c6ad1f9c5a1fd9e03102c76fce49b024dc4d0
Dhyanam <dhyanam2412@gmail.com>
2024-01-15 01:42:11 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,64 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from twilio.rest import Client
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+# Define Verify_otp() function
+@app.route('/login' , methods=['POST'])
+def verify_otp():
+    username = request.form['username']
+    password = request.form['password']
+    mobile_number = request.form['number']
+
+    if username == 'verify' and password == '12345':   
+        account_sid = 'ACd9373aca7768a4b5d99084a67033e77d'
+        auth_token = '74ff61896d7386730bf7a1bd7a12b1ad'
+        client = Client(account_sid, auth_token)
+
+        verification = client.verify \
+            .services('VA49f17f35bd1ac1dd9c06a5eaae39cea5') \
+            .verifications \
+            .create(to=mobile_number, channel='sms')
+
+        print(verification.status)
+        return render_template('otp_verify.html')
+    else:
+        return render_template('user_error.html')
+
+
+
+@app.route('/otp', methods=['POST'])
+def get_otp():
+    print('processing')
+
+    received_otp = request.form['received_otp']
+    mobile_number = request.form['number']
+
+    account_sid = 'ACd9373aca7768a4b5d99084a67033e77d'
+    auth_token = '74ff61896d7386730bf7a1bd7a12b1ad'
+    client = Client(account_sid, auth_token)
+                                            
+    verification_check = client.verify \
+        .services('VA49f17f35bd1ac1dd9c06a5eaae39cea5') \
+        .verification_checks \
+        .create(to=mobile_number, code=received_otp)
+    print(verification_check.status)
+
+    if verification_check.status == "pending":
+        return render_template('otp_verify.html')    # Write code here
+    else:
+        return redirect("https://class-272-4yeo.onrender.com/")
+
+
+if __name__ == "__main__":
+    app.run()
+
